02e1bb977c683bbb0060b7a85beef21e5668928b
Morteza Giti <mortezagiti1990@gmail.com>
2023-09-26 15:42:44 +0330

    Final commit.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleApp1;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var unsorted = new Queue<Queue<int>>();
+        long listLength = 100000;
+        int maxSize = 1000,
+            numSorts = 10;
+
+        Random rand = new Random();
+
+        Console.WriteLine("Creating list of size " + listLength);
+
+        for (int i = 0; i < listLength; ++i)
+        {
+            var l = new Queue<int>();
+            l.Enqueue(rand.Next(maxSize));
+            unsorted.Enqueue(l);
+        }
+
+        Console.WriteLine("Staring Normal Sorting for " + listLength + " elements from 0 to " + maxSize);
+
+        long last = 0;
+        Stopwatch normalSortingStopWatch = new Stopwatch();
+        for (int i = 0; i < numSorts; ++i)
+        {
+            var pending = new Queue<Queue<int>>();
+            foreach (Queue<int> q in unsorted)
+            {
+                pending.Enqueue(new Queue<int>(q));
+            }
+            normalSortingStopWatch.Start();
+            Queue<int> sorted = Sorter.MergeSort(pending);
+            normalSortingStopWatch.Stop();
+            Console.WriteLine("MergeSort " + i + " - " + (normalSortingStopWatch.ElapsedMilliseconds - last) + "ms");
+            last = normalSortingStopWatch.ElapsedMilliseconds;
+        }
+        Console.WriteLine("Normal Sorting: " + normalSortingStopWatch.ElapsedMilliseconds / numSorts + "ms (AVG)");
+
+        long lastParallel = 0;
+        Stopwatch parallelSortingStopWatch = new Stopwatch();
+        Console.WriteLine("Starting Parallel for " + listLength + " elements from 0 to " + maxSize);
+        for (int i = 0; i < numSorts; ++i)
+        {
+            var pending = new Queue<Queue<int>>();
+            foreach (Queue<int> q in unsorted)
+            {
+                pending.Enqueue(new Queue<int>(q));
+            }
+            parallelSortingStopWatch.Start();
+            Queue<int> sortedParallel = Sorter.ParallelMergeSort(pending);
+            parallelSortingStopWatch.Stop();
+            Console.WriteLine("ParallelMergeSort " + i + " - " + (parallelSortingStopWatch.ElapsedMilliseconds - lastParallel) + "ms");
+            lastParallel = parallelSortingStopWatch.ElapsedMilliseconds;
+        }
+        Console.WriteLine("Parallel Sorting: " + parallelSortingStopWatch.ElapsedMilliseconds / numSorts + "ms (AVG)");
+        Console.ReadKey();
+    }
+}
---
+++ ConsoleApp1/Sorter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Sorter
+    {
+        public static Queue<int> ParallelMergeSort(Queue<Queue<int>> pending)
+        {
+            var working = 0;
+            var tasks = Enumerable.Range(0, Environment.ProcessorCount).Select(_ =>
+                Task.Factory.StartNew(() =>
+                {
+                    Queue<int> l1, l2, l3;
+                    while (pending.Count >= 2)
+                    {
+                        lock (pending)
+                        {
+                            Interlocked.Increment(ref working);
+                            l1 = pending.Dequeue();
+                            l2 = pending.Dequeue();
+                        }
+
+                        l3 = new Queue<int>();
+
+                        while (l1.Count > 0 || l2.Count > 0)
+                        {
+                            if (l1.Count > 0 && l2.Count > 0)
+                            {
+                                if (l1.First() <= l2.First())
+                                {
+                                    l3.Enqueue(l1.Dequeue());
+                                }
+                                else
+                                {
+                                    l3.Enqueue(l2.Dequeue());
+                                }
+                            }
+                            else if (l1.Count > 0)
+                            {
+                                while (l1.Count > 0)
+                                {
+                                    l3.Enqueue(l1.Dequeue());
+                                }
+                            }
+                            else if (l2.Count > 0)
+                            {
+                                while (l2.Count > 0)
+                                {
+                                    l3.Enqueue(l2.Dequeue());
+                                }
+                            }
+                        }
+
+                        lock (pending)
+                        {
+                            pending.Enqueue(l3);
+                            Interlocked.Decrement(ref working);
+                        }
+
+                        while (Thread.VolatileRead(ref working) > 0 && !(pending.Count >= 2))
+                        {
+                            Thread.Sleep(5);
+                        }
+                    }
+
+                }, TaskCreationOptions.LongRunning)
+            );
+
+            Task.WaitAll(tasks.ToArray());
+            return pending.Dequeue();
+        }
+        public static Queue<int> MergeSort(Queue<Queue<int>> pending)
+        {
+            Queue<int> l1, l2, l3;
+            while (pending.Count >= 2)
+            {
+                l1 = pending.Dequeue();
+                l2 = pending.Dequeue();
+                l3 = new Queue<int>();
+
+                while (l1.Count > 0 || l2.Count > 0)
+                {
+                    if (l1.Count > 0 && l2.Count > 0)
+                    {
+                        if (l1.First() <= l2.First())
+                        {
+                            l3.Enqueue(l1.Dequeue());
+                        }
+                        else
+                        {
+                            l3.Enqueue(l2.Dequeue());
+                        }
+                    }
+                    else if (l1.Count > 0)
+                    {
+                        while (l1.Count > 0)
+                        {
+                            l3.Enqueue(l1.Dequeue());
+                        }
+                    }
+                    else if (l2.Count > 0)
+                    {
+                        while (l2.Count > 0)
+                        {
+                            l3.Enqueue(l2.Dequeue());
+                        }
+                    }
+                }
+                pending.Enqueue(l3);
+            }
+
+            return pending.Dequeue(); ;
+        }
+
+    }
+}
